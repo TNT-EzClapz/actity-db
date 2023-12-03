d6c1468ba3621fe96b4c96ebdff4d08abbdd1269
Yosyas <yosyaswoldeab@gmail.com>
2023-12-03 03:04:01 +0100

    kinda works

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const path = require('path');
+const pasth = require('path');
+const bcrypt = require("bcrypt");
+const collection = require("./config");
 
 const app = express();
+
+app.set('view engine', 'ejs')
+
+
 const router = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res)=>{
+    res.render("login");
+});
+
+app.get("/signup", (req, res)=>{
+    res.render("signup");
+});
+
 app.use('/', router);
 
 module.exports = app;
--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
+const connect = mongoose.connect("mongodb://mongodb+srv://philip:free@public.nsauw4d.mongodb.net/ActivityDB?retryWrites=true&w=majority");
+connect.then(()=>{
+    console.log("ActivityDB connected successfully");
+})
+.catch(()=>{
+    console.log("ActivityDB cannot be connected");
+});
 
-const url = "mongodb+srv://philip:free@public.nsauw4d.mongodb.net/ActivityDB?retryWrites=true&w=majority";
-const client = new MongoClient(url);
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
 
-client.connect();
+const collection = new mongoose.model("users", LoginSchema)
 
-mongoose.connect(url).then(() => console.log('MongoDB Connected...'));
-
-exports.ping = (req, res) => {
-    res.send('pong');
-};
-
+module.exports = collection;
